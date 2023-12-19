27f8d105260c3ae7631825112ede4786a0429e15
StarlightSangdan <danju@starlight-sangdan.com>
2023-12-19 20:04:36 +0900

    done

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # UI Library Example
+
+## Usage
+
+-   install package first
+
+```bash
+npm i ui-lib-tester
+```
+
+-   execute npx for building components
+
+```bash
+npx ui-lib-action@latest
+```
--- bin/index.js
+++ bin/index.js
@@ -2,17 +2,29 @@
 
 const path = require("path");
 const fs = require("fs");
+const child = require("child_process");
 
 const CWD = process.cwd();
-
 const dir = fs.readdirSync(CWD);
 
+// file io
 if (dir.includes("components")) {
     console.error("components directory already exists.");
-    process.exit(1);
 } else {
     fs.mkdirSync(path.resolve(CWD, "components"));
     console.log("components directory created successfully.");
 }
+
+const initialDir = fs.readdirSync(
+    path.join(CWD, "node_modules", "ui-lib-tester", "components")
+);
+
+initialDir.forEach((file) => {
+    let content = fs.readFileSync(
+        path.resolve("node_modules", "ui-lib-tester", "components", file),
+        "utf-8"
+    );
+    fs.writeFileSync(path.resolve(CWD, "components", file), content);
+});
 
 process.exit(0);
